681961dba9c19717a651ea6ac803bf918ccf50a8
deusse <mbevakl@gmail.com>
2013-04-13 12:50:33 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mapicha::Application.config.secret_token = '9ee9e9d6161504e08dda48806b4cb4f46fc4a42f70156466cdf64f41c64c544d5b22fad71e65a3a62254a631d7b8fc8caf4eafdc5c0c44cbf6919a5abd400871'
